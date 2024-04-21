2f38df730db0e0eb1f7b4f8b9d8cecb5049a9904
xuekaizhu_linlab <xuekaizhu0@gmail.com>
2024-04-21 16:40:51 +0800

    update

--- no_use_files/todo_version/deepspeed_inference/inference_with_dp.sh
+++
@@ -1,4 +0,0 @@
-deepspeed --num_gpus 2 inference_test.py \
-    --model_name_or_path /root/zhuxuekai/comparison_methods_of_HITL/pre_trained_model/facebook/opt-1.3b \
-    --batch_size 2 \
-    --ds_inference
